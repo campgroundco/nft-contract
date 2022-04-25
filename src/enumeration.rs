93c02fc611138b25a09097804d82f384bcf7f1d9
andreespirela <andreespirela@outlook.com>
2022-04-25 14:22:55 -0400

    Fix pagination on nft tokens

--- src/enumeration.rs
+++ src/enumeration.rs
@@ -49,7 +49,7 @@ impl Contract {
             //skip to the index we specified in the start variable
             .skip(start as usize)
             //take the first "limit" elements in the vector. If we didn't specify a limit, use 50
-            .take(limit.unwrap_or(50) as usize)
+            .take(limit.map(|l| l as usize).unwrap_or(usize::MAX))
             //we'll map the token IDs which are strings into Json Tokens
             .map(|token_id| self.trail_ticket(token_id.clone()).unwrap())
             //since we turned the keys into an iterator, we need to turn it back into a vector to return
