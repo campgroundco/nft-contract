d088f5109b8117107345f4f78435673f54a67909
andreespirela <andreespirela@outlook.com>
2022-04-27 17:52:55 -0400

    Fixing integration tests

--- src/sandbox/testCore.js
+++ src/sandbox/testCore.js
@@ -22,7 +22,7 @@ const getConfig = (env, port) => {
 
 const contractMethods = {
     viewMethods: ["series_exists", "get_owner", "get_trail_by_id", "is_owner", "get_trail_business", "get_all_trails_by_owner", "get_current_fee", "get_fee_percentage", "get_treasury_address", "is_creator", "get_trail_by_id_optional", "get_all_trails_by_creator"],
-    changeMethods: ["create_trail_series", "buy_series", "nft_mint", "nft_metadata", "trail_ticket", "new_default_meta"],
+    changeMethods: ["create_trail_series", "nft_buy_series", "nft_mint", "nft_metadata", "trail_ticket", "new_default_meta"],
 };
 
 module.exports = {
