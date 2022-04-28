036419721df001b5c412eec4b7ab066713023b3e
andreespirela <andreespirela@outlook.com>
2022-04-28 16:06:14 -0400

    Implementing transfer business logic

--- src/internal.rs
+++ src/internal.rs
@@ -108,6 +108,56 @@ impl Contract {
             .insert(account_id, &trails_set);
     }
 
+    //remove a token from an owner (internal method and can't be called directly via CLI).
+    pub(crate) fn internal_remove_trail_from_owner(
+        &mut self,
+        account_id: &AccountId,
+        trail_id: &TrailIdAndCopyNumber,
+    ) {
+        //we get the set of tokens that the owner has
+        let mut tokens_set = self
+            .tokens_per_owner
+            .get(account_id)
+            //if there is no set of tokens for the owner, we panic with the following message:
+            .expect("Account does not own any tokens");
+
+        //we remove the the token_id from the set of tokens
+        tokens_set.remove(trail_id);
+
+        //if the token set is now empty, we remove the owner from the tokens_per_owner collection
+        if tokens_set.is_empty() {
+            self.tokens_per_owner.remove(account_id);
+        } else {
+            //if the token set is not empty, we simply insert it back for the account ID.
+            self.tokens_per_owner.insert(account_id, &tokens_set);
+        }
+    }
+
+    pub(crate) fn internal_transfer(
+        &mut self,
+        sender_id: &AccountId,
+        receiver_id: &AccountId,
+        trail_id: &TrailIdAndCopyNumber,
+        //we introduce an approval ID so that people with that approval ID can transfer the token
+        approval_id: Option<u64>,
+        memo: Option<String>,
+    ) -> TrailBusiness {
+        let trail = self.tokens_by_id.get(trail_id).expect("Trail does not exist");
+
+        assert_eq!(sender_id, &trail.owner_id, "Only owner can transfer trail");
+        assert_ne!(receiver_id, &trail.owner_id, "The trail owner and receiver must be different");
+
+        self.internal_remove_trail_from_owner(&trail.owner_id, trail_id);
+        self.internal_add_trail_to_owner(receiver_id, trail_id);
+
+        let mut new_trail_business = trail.clone();
+        new_trail_business.owner_id = receiver_id.clone();
+
+        self.tokens_by_id.insert(trail_id, &new_trail_business);
+
+        new_trail_business
+    }
+
     pub(crate) fn panic_if_not_owner(&self) {
         if !self.is_caller_contract_owner() {
             panic!("Campground: Only contract owner can execute")
