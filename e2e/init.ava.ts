1c0ebf03ca7a1316e01a224132795fcf961a6077
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-05-11 09:56:08 +0200

    Refactor setup into own module

--- e2e/init.ava.ts
+++ e2e/init.ava.ts
@@ -1,17 +1,8 @@
 import test from 'ava';
-import BN from 'bn.js';
-import { Account, connect, Contract, KeyPair, keyStores, Near } from 'near-api-js';
-import { formatNearAmount, parseNearAmount } from 'near-api-js/lib/utils/format';
-import { getConfig } from './lib/config';
-import { deployContract, initContract } from './lib/deploy';
+import { Account, Contract } from 'near-api-js';
+import { parseNearAmount } from 'near-api-js/lib/utils/format';
 import * as ITO from '../ito';
-import { createFSKeyStore, getAccount } from './lib/store';
-
-// function NEAR(amount: string) {
-//     return new BN(parseNearAmount(amount)!);
-// }
-
-const env = process.argv[2];
+import { setup } from './lib/setup';
 
 let ito: Account;
 let owner: Contract & ITO.Contract;
@@ -19,30 +10,7 @@ let alice: Contract & ITO.Contract;
 let bob: Contract & ITO.Contract;
 
 test.before(async _t => {
-    const near = await connect({
-        keyStore: createFSKeyStore(),
-        ...getConfig(env),
-    });
-
-    function contract(account: Account): Contract & ITO.Contract {
-        return new Contract(account, ito.accountId, ITO.ContractMethods) as any;
-    }
-
-    ito = await deployContract(near, 'ito', 'target/wasm32-unknown-unknown/release/ito_contract.wasm');
-    owner = contract(await getAccount('owner', near));
-    alice = contract(await getAccount('alice', near));
-    bob = contract(await getAccount('bob', near));
-
-    await initContract<ITO.Contract, keyof ITO.Contract>(owner,
-        {
-            func: 'new_default_meta',
-            args: {
-                owner_id: owner.account.accountId,
-                treasury_id: owner.account.accountId
-            },
-        }
-    );
-
+    [ito, owner, alice, bob] = await setup();
 });
 
 test('contract should return correct metadata', async t => {
---
+++ e2e/lib/setup.ts
@@ -0,0 +1,36 @@
+import { Account, connect, Contract } from "near-api-js";
+import { getConfig } from "./config";
+import { deployContract, initContract } from "./deploy";
+import { createFSKeyStore, getAccount } from "./store";
+import * as ITO from '../../ito';
+
+const env = process.argv[2];
+
+export async function setup(): Promise<[Account, ...(Contract & ITO.Contract)[]]> {
+    const near = await connect({
+        keyStore: createFSKeyStore(),
+        ...getConfig(env),
+    });
+
+    const ito = await deployContract(near, 'ito', 'target/wasm32-unknown-unknown/release/ito_contract.wasm');
+
+    async function contract(name: string): Promise<Contract & ITO.Contract> {
+        const account = await getAccount(name, near);
+        return new Contract(account, ito.accountId, ITO.ContractMethods) as any;
+    }
+
+    const accounts = await Promise.all(['owner', 'alice', 'bob', 'charlie'].map(name => contract(name)));
+    const owner = accounts[0];
+
+    await initContract<ITO.Contract, keyof ITO.Contract>(owner,
+        {
+            func: 'new_default_meta',
+            args: {
+                owner_id: owner.account.accountId,
+                treasury_id: owner.account.accountId
+            },
+        }
+    );
+
+    return [ito, ...accounts];
+}
