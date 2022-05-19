79b06c42683cdcecee56250744c1e5e9e5debe56
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-05-19 14:27:53 +0200

    Move e2e tests to mocha/chai

--- ava.config.js
+++
@@ -1,8 +0,0 @@
-// require("util").inspect.defaultOptions.depth = 5; // Increase AVA's printing depth
-
-module.exports = {
-    timeout: "300000",
-    files: ["e2e/*.ava.ts"],
-    extensions: ["ts"],
-    require: ["ts-node/register"],
-};
